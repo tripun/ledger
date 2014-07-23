048ee1793d75030440355a32666e2a6a990ec938
tripun <tripun@gmail.com>
2014-07-23 14:17:41 +0530

    debug filters

--- src/filters.cc
+++ src/filters.cc
@@ -69,6 +69,7 @@ void post_splitter::operator()(post_t& post)
 {
   bind_scope_t bound_scope(report, post);
   value_t      result(group_by_expr.calc(bound_scope));
+  DEBUG("amount.parse", "filters.cc:post_splitter ");
 
   if (! result.is_null()) {
     value_to_posts_map::iterator i = posts_map.find(result);
@@ -153,6 +154,7 @@ void truncate_xacts::operator()(post_t& post)
 
 void sort_posts::post_accumulated_posts()
 {
+  DEBUG("amount.parse", "filters.cc: sort_posts");
   std::stable_sort(posts.begin(), posts.end(),
                    compare_items<post_t>(sort_order));
 
